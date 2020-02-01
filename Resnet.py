2e80a90ddb25dfb87735b10ea7d388cffa93474f
yuki75678 <hjim7047@gmail.com>
2020-02-01 18:16:34 +0900

    resnet_v0.6

--- Resnet.py
+++ Resnet.py
@@ -5,23 +5,26 @@ import sys
 
 #combine all resnet layer
 def Resnet_layer(input,strides,conv_dim):
-
+    #put length
     length = len(strides)/3
+
+    #error handling of input
     if float.is_integer(length):
+
         length = int(length)
 
-
-
         print(length)
+        #setup resnet layer input
         resnet_input_and_output = input
 
         for i in range(length):
             strides_1 = strides[i]
             strides_2 = strides[i+1]
             strides_3 = strides[i+2]
-    #return first resnet module output
+            #return  resnet module output
             output = Rb.Resnet_block(resnet_input_and_output,strides_1,strides_2,strides_3,conv_dim[i])
 
+            #setup resnet layer input
             resnet_input_and_output = output
 
     else:
